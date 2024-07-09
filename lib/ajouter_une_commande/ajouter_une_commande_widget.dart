16c974aad915ef98c330a4781e4c65d72ff9a441
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-09 23:48:39 +0000

    Updating to latest FlutterFlow output.

--- lib/ajouter_une_commande/ajouter_une_commande_widget.dart
+++ lib/ajouter_une_commande/ajouter_une_commande_widget.dart
@@ -1,3 +1,4 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
@@ -541,6 +542,10 @@ class _AjouterUneCommandeWidgetState extends State<AjouterUneCommandeWidget> {
                                   destination: _model.textController2.text,
                                   prix:
                                       int.tryParse(_model.textController3.text),
+                                  whoCommandIt: currentUserReference,
+                                  isCommandValidated: false,
+                                  isCommandAccepted: false,
+                                  isCommandRefused: false,
                                 ));
                           },
                           text: 'Enregistrer',
